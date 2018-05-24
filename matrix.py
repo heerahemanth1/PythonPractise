584d47beed0ee7e8d782c73cbb2c5c482901aaf5
Heera Hemanth Bylla <heerahemanth1@gmail.com>
2018-05-24 23:48:00 +0530

    Functionalities of a Matrix, like Transpose, Addition, Subtraction, Multiplication implemented.

--- matrix.py
+++ matrix.py
@@ -11,60 +11,69 @@ class Matrix:
 
     # Returns the number of rows
     def numRows(self):
-    	return self._theGrid.numRows()
+        return self._theGrid.numRows()
 
     # Returns the number of columns
     def numCols(self):
-    	return self._theGrid.numCols()
+        return self._theGrid.numCols()
 
     # Returns the element at (i, j)
     def __getitem__(self, ndxTuple):
-    	return self._theGrid[ndxTuple[0], ndxTuple[1]]
+        return self._theGrid[ndxTuple[0], ndxTuple[1]]
 
     # Sets the value of element at (i, j) to scalar
     def __setitem__(self, ndxTuple, scalar):
-    	self._theGrid[ndxTuple[0], ndxTuple[1]] = scalar
+        self._theGrid[ndxTuple[0], ndxTuple[1]] = scalar
 
     # Scales the matrix by the given scalar
     def scaleBy(self, scalar):
-    	for r in range(self.numRows()):
-    		for c in range(self.numCols()):
-    			self._theGrid[r, c] += scalar
+        for r in range(self.numRows()):
+            for c in range(self.numCols()):
+                self._theGrid[r, c] += scalar
 
     # Creates and returns a new matrix that is the transpose of this
     def transpose(self):
-    	transpose_matrix = Matrix(self.numCols(), self.numRows())
-    	for r in range(self.numRows()):
-    		for c in range(self.numCols()):
-    			transpose_matrix[c, r] = self._theGrid[r, c]
-    	return transpose_matrix
+        transpose_matrix = Matrix(self.numCols(), self.numRows())
+        for r in range(self.numRows()):
+            for c in range(self.numCols()):
+                transpose_matrix[c, r] = self._theGrid[r, c]
+        return transpose_matrix
 
     # Creates and returns a new matrix that results from matrix addition
     def __add__(self, rhsMatrix):
-    	assert self.numRows() == rhsMatrix.numRows() and \
-    			self.numCols() == rhsMatrix.numCols(), \
-    			"Matrix sizes are not compatible for the add operation."
-    	# Create the new matrix
-    	new_matrix = Matrix(self.numRows(), self.numCols())
-    	# Add the matrices
-    	for r in range(self.numRows()):
-    		for c in range(self.numCols()):
-    			new_matrix[r, c] = self._theGrid[r, c] + rhsMatrix._theGrid[r, c]
-    	return new_matrix
+        assert self.numRows() == rhsMatrix.numRows() and \
+                self.numCols() == rhsMatrix.numCols(), \
+                "Matrix sizes are not compatible for the add operation."
+        # Create the new matrix
+        new_matrix = Matrix(self.numRows(), self.numCols())
+        # Add the matrices
+        for r in range(self.numRows()):
+            for c in range(self.numCols()):
+                new_matrix[r, c] = self._theGrid[r, c] + rhsMatrix._theGrid[r, c]
+        return new_matrix
 
     # Matrix Subtraction
     def __sub__(self, rhsMatrix):
-    	assert self.numRows() == rhsMatrix.numRows() and \
-    			self.numCols() == rhsMatrix.numCols(), \
-    			"Matrix sizes are not compatible for the add operation."
-    	# Create the new matrix
-    	new_matrix = Matrix(self.numRows(), self.numCols())
-    	# Add the matrices
-    	for r in range(self.numRows()):
-    		for c in range(self.numCols()):
-    			new_matrix[r, c] = self._theGrid[r, c] - rhsMatrix._theGrid[r, c]
-    	return new_matrix
+        assert self.numRows() == rhsMatrix.numRows() and \
+                self.numCols() == rhsMatrix.numCols(), \
+                "Matrix sizes are not compatible for the add operation."
+        # Create the new matrix
+        new_matrix = Matrix(self.numRows(), self.numCols())
+        # Add the matrices
+        for r in range(self.numRows()):
+            for c in range(self.numCols()):
+                new_matrix[r, c] = self._theGrid[r, c] - rhsMatrix._theGrid[r, c]
+        return new_matrix
 
     # Matrix Multiplication
     def __mul__(self, rhsMatrix):
-    	pass
+        assert self.numCols() == rhsMatrix.numRows(), \
+            "Matrix sizes are not compatible for the add operation."
+        new_matrix = Matrix(self.numRows(), rhsMatrix.numCols())
+        for r in range(self.numRows()):
+            for c in range(rhsMatrix.numCols()):
+                sum = 0
+                for rr in range(rhsMatrix.numRows()):
+                    sum += self._theGrid[r, rr] * rhsMatrix._theGrid[rr, r]
+                new_matrix[r, c] = sum
+        return new_matrix
